f55fe64d4388e90f8792c4915cd382013696843b
lera123w1 <144344259+lera123w1@users.noreply.github.com>
2023-11-03 15:37:32 +0300

    project merge

---
+++ app/src/main/java/com/example/kurkin_library/Book.kt
@@ -0,0 +1,13 @@
+package edu.muvitte.library
+
+import java.util.Date
+import java.util.UUID
+
+data class Book (
+    val bookID: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var author: String = "",
+    var bookDate: Date = Date(),
+    var price: Float = 0.0f,
+    var rented: Boolean = false
+)
--- app/src/main/java/com/example/kurkin_library/MainActivity.kt
+++ app/src/main/java/com/example/kurkin_library/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kurkin_library
+package edu.muvitte.library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
